7f980737281f4d8443f2ba09abaa3f1ec77c6778
Adam Barton <adam.barton333@gmail.com>
2018-10-18 00:26:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20181017221250) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+  end
+
 end
